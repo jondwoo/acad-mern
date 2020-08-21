e59ae4d7c5e9db65d6c067b099fa08619a9371e1
Jon <jondwoo@pop-os.localdomain>
2020-08-21 02:08:20 -0700

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,23 @@ import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch,
+  Switch
 } from 'react-router-dom';
 
 import Users from './user/pages/Users';
-import NewPlace from './places/pages/Place';
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path='/' exact>
+        <Route path="/" exact>
           <Users />
         </Route>
-        <Route path='/places/new' exact>
+        <Route path="/places/new" exact>
           <NewPlace />
         </Route>
-        <Redirect to='/' />
+        <Redirect to="/" />
       </Switch>
     </Router>
   );
