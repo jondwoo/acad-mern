1100d033f2e028445d5b1c10ca50f91086d79943
Jon <jondwoo@pop-os.localdomain>
2020-08-23 13:54:22 -0700

    implemented modal

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -4,7 +4,7 @@ import Card from '../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem';
 import './PlaceList.css';
 
-const PlaceList = props => {
+const PlaceList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="place-list center">
@@ -18,7 +18,7 @@ const PlaceList = props => {
 
   return (
     <ul className="place-list">
-      {props.items.map(place => (
+      {props.items.map((place) => (
         <PlaceItem
           key={place.id}
           id={place.id}
