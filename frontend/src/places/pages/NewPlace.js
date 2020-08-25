eb959a7bceb32bd3137b11a46f9f971edce64357
Jon Ham <jondwoo@gmail.com>
2020-08-24 21:54:39 -0700

    validate input

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewPlace.css';
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" element="input" validators={[]} errorText="Please enter valid title"/>
+      <Input
+        type="text"
+        label="Title"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter valid title"
+      />
     </form>
   );
 };
